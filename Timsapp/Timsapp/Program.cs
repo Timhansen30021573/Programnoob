6cf4ad4dcd6c326dc7461a8eb2d30e3095292585
Timothy Hansen <30021573@student.toiohomai.ac.nz>
2019-02-27 10:44:51 +1300

    project

---
+++ Timsapp/Timsapp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timsapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int age;
+
+            string name;
+
+            Console.WriteLine("please enter your age");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine(name+age);
+            Console.ReadLine();
+            
+        
+
+
+        }
+
+
+    }
+}
